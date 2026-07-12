5f45294601519b389d77a745a7b225251c9a8cc1
Ramius1701 <Ramius.Easterwood@gmail.com>
2026-07-12 15:37:43 +1200

    Add files via upload

---
+++ addon-modules/OpenSimTide/Module/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Mono.Addins;
+
+[assembly: Addin("OpenSimTide", "0.2")]
+[assembly: AddinDependency("OpenSim.Region.Framework", OpenSim.VersionInfo.VersionNumber)]
+
+namespace TideModule
+{
+    class Info
+    {
+        public const string VersionNumber = "0.2";
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string location = Assembly.GetExecutingAssembly().Location;
+                return Path.GetDirectoryName(location);
+            }
+        }
+
+    }
+}
